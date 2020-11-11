8f7d38234828f73c75d9be043dbf87877bcd64f0
Michel636 <74297655+Michel636@users.noreply.github.com>
2020-11-11 11:58:37 +0100

    Add files via upload

---
+++ winkelwagen.php
@@ -0,0 +1,19 @@
+<div>
+    Overzicht producten
+    Afbeelding, Naam, aantal, bewerken aantal (-1, +1, aantalbewerken, delete), totale prijs product
+</div>
+<div>
+    Kortingscode
+</div>
+<div>
+    Overzicht prijzen
+</div>
+<div>
+    Terug naar webwinkel
+</div>
+<div>
+    Doorgaan als gast
+</div>
+<div>
+    Doorgaan als klant
+</div>
