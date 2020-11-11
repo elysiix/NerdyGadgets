77436cc37169f94fa505cf1f19a5b7132bab0517
Michel636 <74297655+Michel636@users.noreply.github.com>
2020-11-11 12:01:35 +0100

    Update winkelwagen.php

--- winkelwagen.php
+++ winkelwagen.php
@@ -1,19 +1,30 @@
-<div>
-    Overzicht producten
-    Afbeelding, Naam, aantal, bewerken aantal (-1, +1, aantalbewerken, delete), totale prijs product
+<?php
+include __DIR__ . "/header.php";
+?>
+<div class="IndexStyle">
+    <div>
+        Overzicht producten
+        Afbeelding, Naam, aantal, bewerken aantal (-1, +1, aantalbewerken, delete), totale prijs product
+    </div>
+    <div>
+        Kortingscode
+    </div>
+    <div>
+        Overzicht prijzen
+    </div>
+    <div>
+        Terug naar webwinkel
+    </div>
+    <div>
+        Doorgaan als gast
+    </div>
+    <div>
+        Doorgaan als klant
+    </div>
 </div>
-<div>
-    Kortingscode
-</div>
-<div>
-    Overzicht prijzen
-</div>
-<div>
-    Terug naar webwinkel
-</div>
-<div>
-    Doorgaan als gast
-</div>
-<div>
-    Doorgaan als klant
-</div>
+<?php
+include __DIR__ . "/footer.php";
+?>
+
+
+
