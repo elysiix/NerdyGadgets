30bae331673f630043a7593db9c809c69ccf00dd
elysiix <lars.komin@gmail.com>
2020-11-14 19:20:07 +0100

    update zat 14/11

--- checkout-bezoeker.php
+++ checkout-bezoeker.php
@@ -44,7 +44,34 @@ include "connect.php";
 <body>
 <!-- bestellingscode-->
 <?php
+session_start();
+?>
 
+<table>
+    <thead>
+    <tr>
+        <th>Foto</th>
+        <th>Naam</th>
+        <th>Aantal</th>
+        <th>Prijs</th>
+        <th>Acties</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <?php
+        foreach($_SESSION as $key => $values)
+        {
+        ?>
+    <tr>
+        <td><?php echo $values["name"]?></td>
+        <td><?php echo $values["quantity"]?></td>
+        <td><?php echo $values["price"]?></td>
+        <?php
+        } ?>
+    </tr>
+    </tbody>
+</table>
 ?>
 
 
