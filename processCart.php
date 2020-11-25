44b8b31184f7c0788d6e8a4ead5c3470a0e58a7d
a.pijlgroms <a.pijlgroms@uwboeking.com>
2020-11-25 10:46:47 +0100

    paar items opgepakt vanuit de trello

--- processCart.php
+++ processCart.php
@@ -2,27 +2,41 @@
 
 session_start();
 
-$id = $_POST["hidden_id"];
+$id = $_POST['hidden_id'];
 $name = $_POST["hidden_name"];
 $qty = $_POST["quantity"];
 $price = $_POST["hidden_price"];
+//$image = $_POST['hidden_image'];
 
 
-if($qty > 1)
+if($_SESSION[$name] >= 1)
 {
-    $total = $price * $qty;
+    foreach ($_SESSION as $value)
+    {
+       $test = $value['quantity'] + $qty;
 
-    $product = array("id" => $id, "name" => $name, "quantity"=> $qty, "price" => $total);
+       $total = $price * $qty;
 
-//    $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $product);
+       $product = array("id" => $id, "name" => $name, "quantity"=> $test, "price" => $total);
+
+        $_SESSION[$name] = $product;
+
+        header('location:cartList.php');
+
+    }
+}
+elseif($qty < 1)
+{
+    print("Je kan niet minder dan 1 artikel bestellen klik hier om terug te gaan <br>");
+    print("<a href=addProduct.php>Product toevoegen</a>");
 }
 else
 {
     $product = array("id" => $id, "name" => $name, "quantity"=> $qty, "price" => $price);
 
 //    $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $product);
+
+    $_SESSION[$name] = $product;
+
+    header('location:cartList.php');
 }
-
-$_SESSION[$name] = $product;
-
-header('location:cartList.php');
